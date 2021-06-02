e668521aef3cb6efca4ff916e839bd9c4d6144ce
Arnav Mukhopadhyay <gudduarnav@gmail.com>
2021-06-02 11:56:56 +0530

    a

--- CoWIN_API/example_UA.py
+++
@@ -1,3 +0,0 @@
-from api.useragent import getUA
-
-print("Header=" , getUA())
--- CoWIN_API/example_distlist.py
+++
@@ -1,5 +0,0 @@
-from api.statelist import getDistrictandState
-
-all = getDistrictandState()
-for id, name in all.items():
-    print(id,"=", name)
